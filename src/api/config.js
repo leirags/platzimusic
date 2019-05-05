55725a3b8655882e2a91ae9f8b98cb7dc36be4b8
Ariel Garcia <leirags@hotmail.com>
2019-05-04 23:52:36 -0600

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,12 @@
+//LAST-FM
+//API account created
+//Here are the details of your new API account.
+export default {
+  AppName: 'PlatziMusic',
+  apiKey: '77a9d32d5378b899e5b0c2c45beccfc2',
+  secret:	'449c1ec60f6da67a8e483127b42ac75f',
+  registeredTo: 'leirags'
+}
+// Please keep a copy of these details, either by
+// saving the page or taking a screenshot,
+// as we don't currently provide the ability to look up accounts.
